6b70c3ee2524423219fbb3f333d5922363b930f3
bongerka <smartyom197@gmail.com>
2022-10-11 04:12:41 +0300

    edit tests

--- tests/test_process.py
+++ tests/test_process.py
@@ -5,6 +5,24 @@ alphabet: set = {"a", "b", "c", "1"}
 operations: set = {".", '+', '*'}
 
 
+def test_answer_1():
+    instance = RegularExpressionHandling('ab+c.aba.*.bac.+.+*', 3, 2, alphabet, operations)
+    result = instance.are_exist_necessary_words()
+    assert result is True
+
+
+def test_answer_2():
+    instance = RegularExpressionHandling('a1+1.aba.*.bac.+.+*', 3, 2, alphabet, operations)
+    result = instance.are_exist_necessary_words()
+    assert result is True
+
+
+def test_answer_3():
+    instance = RegularExpressionHandling('acb..bab.c.*.ab.ba.+.+*a.', 3, 0, alphabet, operations)
+    result = instance.are_exist_necessary_words()
+    assert result is False
+
+
 def test_initiation_1():
     with pytest.raises(AttributeError) as e:
         RegularExpressionHandling("ad.b+)", 2, 1, alphabet, operations)
@@ -19,74 +37,56 @@ def test_initiation_2():
 
 
 def test_sum_1():
-    expr1 = [1, 0, 3]
-    expr2 = [-1, 2, 1]
+    expr1 = [1, 1, 1]
+    expr2 = [0, 1, 1]
     my_func = RegularExpressionHandling._RegularExpressionHandling__sum
     res = my_func(expr1, expr2)
-    assert res == [1, 0, 1]
+    assert res == [1, 1, 1]
 
 
 def test_sum_2():
-    expr1 = [-1, 0, -1, -1, -1]
-    expr2 = [1, 0, 1, 0, 1]
+    expr1 = [0, 1, 0, 0, 0]
+    expr2 = [1, 1, 1, 1, 1]
     my_func = RegularExpressionHandling._RegularExpressionHandling__sum
     res = my_func(expr1, expr2)
     assert res == expr2
 
 
 def test_concat_1():
-    expr1 = [0, 0, 0]
-    expr2 = [-1, 0, -1]
+    expr1 = [1, 1, 1]
+    expr2 = [0, 1, 0]
     my_func = RegularExpressionHandling._RegularExpressionHandling__concatenation
     res = my_func(expr1, expr2)
-    assert res == [1, 0, 0]
+    assert res == [1, 1, 1]
 
 
 def test_concat_2():
-    expr1 = [4, 7, 3, -1, 1]
-    expr2 = [3, -1, 2, 1, 5]
+    expr1 = [1, 1, 1, 0, 1]
+    expr2 = [1, 0, 1, 1, 1]
     my_func = RegularExpressionHandling._RegularExpressionHandling__concatenation
     res = my_func(expr1, expr2)
-    assert res == [5, 4, 3, 5, 4]
+    assert res == [1, 1, 1, 1, 1]
 
 
-def test_clini_star_1():
-    expr = [-1, 0, 3, 2]
+def test_kleene_star_1():
+    expr = [0, 1, 1, 1]
     my_func = RegularExpressionHandling._RegularExpressionHandling__kleene_star
     res = my_func(expr)
-    assert res == [0, 0, 0, 0]
+    assert res == [1, 1, 1, 1]
 
 
-def test_clini_star_2():
-    expr = [2, 1]
+def test_kleene_star_2():
+    expr = [1, 1]
     my_func = RegularExpressionHandling._RegularExpressionHandling__kleene_star
     res = my_func(expr)
-    assert res == [0, 1]
+    assert res == [1, 1]
 
 
-def test_clini_star_3():
-    expr = [-1, 0, -1]
+def test_kleene_star_3():
+    expr = [0, 1, 0]
     my_func = RegularExpressionHandling._RegularExpressionHandling__kleene_star
     res = my_func(expr)
-    assert res == [0, 0, 0]
-
-
-def test_clni_star_3():
-    instance = RegularExpressionHandling('ab+c.aba.*.bac.+.+*', 3, 2, alphabet, operations)
-    result = instance.are_exist_necessary_words()
-    assert result is True
-
-
-def test_clini_str_3():
-    instance = RegularExpressionHandling('a1+1.aba.*.bac.+.+*', 3, 2, alphabet, operations)
-    result = instance.are_exist_necessary_words()
-    assert result is True
-
-
-def test_ini_star_3():
-    instance = RegularExpressionHandling('acb..bab.c.*.ab.ba.+.+*a.', 3, 0, alphabet, operations)
-    result = instance.are_exist_necessary_words()
-    assert result is False
+    assert res == [1, 1, 1]
 
 
 if __name__ == "__main__":
